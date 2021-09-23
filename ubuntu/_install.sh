3b03e31a8ebb80d3afe3abff543fb818b07626f9
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-09-23 20:30:35 +0200

    _install.sh: Install zsh and Oh My Zsh

--- ubuntu/_install.sh
+++ ubuntu/_install.sh
@@ -5,11 +5,14 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install packages
-sudo apt install -y sl cmatrix cowsay figlet toilet ranger
+sudo apt install -y cmatrix cowsay figlet ranger sl toilet zsh
 
 # Add the NodeSource repository and install Node 14
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt install -y nodejs
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Run symlink script
 ~/.dotfiles/ubuntu/_symlink.sh
