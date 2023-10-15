f9b6c94e1143ff24403abbe311e6fb482ce4ce03
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2023-10-15 11:33:10 +0200

    motd.sh: Reduce messages, only use script font

--- macos/motd.sh
+++ macos/motd.sh
@@ -1,42 +1,20 @@
 #!/bin/bash
 
 messages=(
-"Yo yo yo"
-"Hey there :)"
-"Welcome back"
-"Epic gamer"
-"Haxxor"
-"sup bicth?"
-"Hello fukco"
-"Have a nice day!"
-"Breaching the firewall"
-"Hacking the mainframe"
-"lol xd"
-"u suck lol"
-"hewwo uwu~"
-"made changes"
-"gitgnore"
-"z-index: 9999;"
-"DROP TABLE students;"
-"rm -f *"
-"npm start"
-"NullPointerException"
-"Lorem ipsum"
-"Sample Text"
-"Placeholder text"
-"\^o^/"
-)
-
-styles=(
-"figlet -f big -c "
-"figlet -f script -c "
-"figlet -f slant -c "
+  "Hey there :)"
+  "Welcome back"
+  "Epic gamer"
+  "Have a nice day!"
+  "Breaching the firewall"
+  "Hacking the mainframe"
+  "made changes"
+  "gitgnore"
+  "z-index: 9999;"
+  "Lorem ipsum"
 )
 
 message=${messages[$RANDOM % ${#messages[@]}]}
 
-style=${styles[$RANDOM % ${#styles[@]}]}
-
-$style $message
+figlet -c -f script $message
 
 echo
