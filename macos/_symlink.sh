712e2d6b0d6ec06ba7c0ca32fb1c0e3d2973a453
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-02-03 22:06:01 +0100

    macOS: Move .zshrc into zsh directory

--- macos/_symlink.sh
+++ macos/_symlink.sh
@@ -8,5 +8,6 @@ ln -siv "$base/.gitconfig"                   ~/.gitconfig
 ln -siv "$base/.config/micro/bindings.json"  ~/.config/micro/bindings.json
 ln -siv "$base/.config/micro/settings.json"  ~/.config/micro/settings.json
 ln -siv "$base/zsh/.p10k.zsh"                ~/.p10k.zsh
+ln -siv "$base/zsh/.zshrc"                   ~/.zshrc
 
 touch ~/.hushlogin
