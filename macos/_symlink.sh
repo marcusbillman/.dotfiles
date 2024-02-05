819556f7a7c992972aa7bb4cab76a31cc52d197b
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2024-02-05 20:28:06 +0100

    macOS & Ubuntu: Move git and micro dotfiles into directories

--- macos/_symlink.sh
+++ macos/_symlink.sh
@@ -4,9 +4,9 @@ base=~/.dotfiles/macos
 
 #       Target path                          Symlink path
 ln -siv "$base/macos/DefaultKeyBinding.dict" ~/Library/KeyBindings/DefaultKeyBinding.dict
-ln -siv "$base/.gitconfig"                   ~/.gitconfig
-ln -siv "$base/.config/micro/bindings.json"  ~/.config/micro/bindings.json
-ln -siv "$base/.config/micro/settings.json"  ~/.config/micro/settings.json
+ln -siv "$base/git/.gitconfig"               ~/.gitconfig
+ln -siv "$base/micro/bindings.json"          ~/.config/micro/bindings.json
+ln -siv "$base/micro/settings.json"          ~/.config/micro/settings.json
 ln -siv "$base/zsh/.p10k.zsh"                ~/.p10k.zsh
 ln -siv "$base/zsh/.zshrc"                   ~/.zshrc
 
--- ubuntu/_symlink.sh
+++ ubuntu/_symlink.sh
@@ -2,11 +2,11 @@
 
 base=~/.dotfiles/ubuntu
 
-#       Target path                          Symlink path
-ln -siv "$base/.gitconfig"                   ~/.gitconfig
-ln -siv "$base/.config/micro/bindings.json"  ~/.config/micro/bindings.json
-ln -siv "$base/.config/micro/settings.json"  ~/.config/micro/settings.json
-ln -siv "$base/zsh/.p10k.zsh"                ~/.p10k.zsh
-ln -siv "$base/zsh/.zshrc"                   ~/.zshrc
+#       Target path                 Symlink path
+ln -siv "$base/git/.gitconfig"      ~/.gitconfig
+ln -siv "$base/micro/bindings.json" ~/.config/micro/bindings.json
+ln -siv "$base/micro/settings.json" ~/.config/micro/settings.json
+ln -siv "$base/zsh/.p10k.zsh"       ~/.p10k.zsh
+ln -siv "$base/zsh/.zshrc"          ~/.zshrc
 
 touch ~/.hushlogin
